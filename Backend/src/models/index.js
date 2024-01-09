f3d90e8ac198dc72371ae7cffb973bca6bdf267d
Awudu-Jamal1 <78368357+Awudu-Jamal1@users.noreply.github.com>
2024-01-09 07:59:44 +0000

    revert

--- Backend/src/models/index.js
+++ Backend/src/models/index.js
@@ -8,12 +8,12 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
-
+const dotenv =require()
 let sequelize;
-if ( config && config.use_env_variable) {
+if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.DB_USER, process.DB_PASSWORD, config);
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
